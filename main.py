5722aa510db9cd86bcd6e220a96cb81ac9783c2c
Chamath Jayarathne <chamathsocial@gmail.com>
2023-09-18 18:09:18 -0400

    feat: Add comments

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import numpy as np
 
+# Size of board
 ROWS = 6
 COLS = 7
 
 
+# Create board using numpy
 def create_board():
     return np.zeros((ROWS, COLS), "int32")
 
 
+# Add piece to location chosen by player
 def drop_piece(board, row, col, player):
     board[row][col] = player
 
 
+# Check if column is full
 def is_valid_location(board, col):
     return board[0][col] == 0
 
 
+# Get which row to add piece to
 def get_next_open_row(board, col):
     for row in range(ROWS-1, -1, -1):
         if board[row][col] == 0:
             return row
 
 
+# Check for winning move
 def winning_move(board, player):
     # Horizontal
     for col in range(COLS - 3):
@@ -48,6 +54,7 @@ def winning_move(board, player):
                 return True
 
 
+# Print board
 def print_board():
     print("\n")
     print(board)
@@ -60,6 +67,7 @@ turn = 0
 while not game_over:
     print_board()
 
+    # Check for errors
     try:
         # Player 1 input
         if turn % 2 == 0:
@@ -77,7 +85,6 @@ while not game_over:
         print("Pick a number between 0 and 6")
 
     else:
-
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             player = turn % 2 + 1
